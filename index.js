0cfbb81ff6e466e4a44196c34404dd9940edc5d2
IvetteMery <ivette.mery@gmail.com>
2020-09-29 23:42:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const player = document.querySelector('.player')
+const puntaje = document.querySelector('#puntaje')
+const tiempoDeJuego = document.querySelector('#tiempo')
+
+let puntos = 0;
+let tiempo = 30;
+let necesarios =45 ;
+
+player.onclick = () => {
+    puntaje.textContent = necesarios + " Clicks Necesarios " + "/" + " Clicks: " + puntos++;
+    randNUm = Math.round(Math.random() * 400);
+    randNUm2 = Math.round(Math.random() * 400);
+    player.style.marginTop = randNUm + "px"
+    player.style.marginLeft = randNUm2 + "px"
+   
+
+
+}
+
+
+let restarTiempo = () => {
+    tiempoDeJuego.innerHTML = "Tiempo:" + tiempo--;
+    if (tiempo == 0 && puntos >=30) {
+        puntaje.textContent = "Ganaste!!"
+        if(tiempo<=0){
+            myStopFunction()
+             tiempoDeJuego.textContent
+             = "Tiempo: 0"
+        }
+    } else if (tiempo == 0 && puntos < 30) {
+        puntaje.textContent = "Lo Siento Perdiste "
+        if(tiempo<=0){
+            myStopFunction()
+             tiempoDeJuego.textContent
+             = "Tiempo: 0"
+        }
+    }
+    
+
+}
+
+
+var myVar =setInterval(restarTiempo, 1000);
+
+
+function myStopFunction() {
+    clearInterval(myVar);
+  }
